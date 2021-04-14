fe191478d725e8ea25e0a371bcdde283f69e5370
moczkowicz <moczkowicz@objectivity.co.uk>
2021-04-14 14:36:04 +0100

    add lighthouse servre address

--- bulletin-board-app/lighthouserc.js
+++ bulletin-board-app/lighthouserc.js
@@ -9,7 +9,7 @@ module.exports = {
         upload: {
             //target: 'temporary-public-storage', - Google
             target: 'lhci',
-            serverBaseUrl: 'http://20.76.42.71:9001/',
+            serverBaseUrl: 'http://20.72.129.91:9001/',
             token: '17f9e085-5287-418d-8ad5-d56fdd99c72e', // could also use LHCI_TOKEN variable instead
             ignoreDuplicateBuildFailure: true //to be able to run w/o git commit
         },
