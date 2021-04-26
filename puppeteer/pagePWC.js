06853e627fde4a29e10d8cd9688bdded6a4d9726
moczkowicz <moczkowicz@objectivity.co.uk>
2021-04-26 14:51:44 +0100

    export function

--- puppeteer/pagePWC.js
+++ puppeteer/pagePWC.js
@@ -1,8 +1,36 @@
-/**
- * @param {puppeteer.Browser} browser
- * @param {{url: string, options: LHCI.CollectCommand.Options}} context
- */
- module.exports = async (browser, context) => {
-    const page = await browser.newPage();
+const puppeteer = require('puppeteer');
 
-  };
+
+module.exports = async() => {
+    const browser = await puppeteer.launch({headless: false, args: ['--start-maximized'], defaultViewport: null,
+     executablePath: '/usr/bin/google-chrome-stable'
+    // executablePath: 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+     });
+    const page = await browser.newPage();
+    const url = "https://www.pwc.com/";
+
+    await page.goto(url);
+    console.log("open page: " + url);
+
+    //locators
+    const industriesTab = "//a[@role='tab']/span[contains(text(), 'Industries')]";
+    const servicesTab = "//a[@role='tab']/span[contains(text(), 'Services')]";
+    const todaysIssuesTab = "//a[@role='tab']/span[contains(text(), 'issues')]";
+
+    //Clicks on tabs
+    await page.waitForXPath(industriesTab);
+    var element = await page.$x(industriesTab);
+    await element[0].click()
+
+    await page.waitForXPath(servicesTab);
+    var element = await page.$x(servicesTab);
+    await element[0].click()
+
+    await page.waitForXPath(todaysIssuesTab);
+    var element = await page.$x(todaysIssuesTab);
+    await element[0].click()
+
+    await page.close();
+
+    console.log("script done");
+};
